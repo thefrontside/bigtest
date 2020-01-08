3627de2fab2e939bdaf967b58711eac3ea621285
Jonas Nicklas <jonas@jnicklas.com>
2020-01-08 13:10:05 +0100

    Add tests for harness, app and proxy

--- test/orchestrator.test.ts
+++ test/orchestrator.test.ts
@@ -39,4 +39,60 @@ describe('orchestrator', () => {
       expect(body).toContain('<title>BigTest</title>');
     });
   });
+
+  describe('retrieving harness', () => {
+    let response: Response;
+    let body: string;
+    beforeEach(async () => {
+      response = await actions.get('http://localhost:24104/harness.js');
+      body = await response.text();
+    });
+
+    it('responds successfully', () => {
+      expect(response.ok).toEqual(true);
+    });
+
+    it('returns the harness script', () => {
+      // TODO: we should probably assert on something more sensible here, but on what?
+      expect(body).toContain('hello from harness');
+    });
+  });
+
+  describe('retrieving app', () => {
+    let response: Response;
+    let body: string;
+    beforeEach(async () => {
+      response = await actions.get('http://localhost:24100/');
+      body = await response.text();
+    });
+
+    it('responds successfully', () => {
+      expect(response.ok).toEqual(true);
+    });
+
+    it('serves the application', () => {
+      expect(body).toContain('<title>React TodoMVC Example</title>');
+    });
+  });
+
+  describe('retrieving app via proxy', () => {
+    let response: Response;
+    let body: string;
+    beforeEach(async () => {
+      response = await actions.get('http://localhost:24101/');
+      body = await response.text();
+    });
+
+    it('responds successfully', () => {
+      expect(response.ok).toEqual(true);
+    });
+
+    it('proxies to the application', () => {
+      expect(body).toContain('<title>React TodoMVC Example</title>');
+    });
+
+    it('injects the harness script tag', () => {
+      expect(body).toContain('<script src="http://localhost:24104/harness.js"></script>');
+    });
+  });
 });
