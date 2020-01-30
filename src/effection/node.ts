e471b3d7d8781b5139d4e7ef453e929318713e88
Jonas Nicklas <jonas@jnicklas.com>
2020-01-30 13:19:55 +0100

    Extract a "main" function which sets up signal interrupts
    See https://github.com/thefrontside/effection.js/issues/68

---
+++ src/effection/node.ts
@@ -0,0 +1,24 @@
+import { main as effectionMain, Context, Operation } from 'effection';
+
+export function main(operation: Operation): Context {
+  return effectionMain(({ context }) => {
+    let mainContext = context as any; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+    mainContext.spawn(function*() {
+      try {
+        yield operation;
+      } catch(e) {
+        console.error(e);
+        throw(e);
+      }
+    });
+
+    let interrupt = () => { mainContext.halt()};
+
+    process.on('SIGINT', interrupt);
+
+    mainContext.ensure(() => {
+      process.off('SIGINT', interrupt);
+    });
+  });
+}
