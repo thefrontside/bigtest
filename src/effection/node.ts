acdb93de4ee5e846b303e8438ab0255e4e696e1f
Charles Lowell <cowboyd@frontside.io>
2020-02-06 15:45:10 -0600

    ♻Use new and improved effection apis

--- src/effection/node.ts
+++ src/effection/node.ts
@@ -1,24 +1,20 @@
 import { main as effectionMain, Context, Operation } from 'effection';
 
 export function main(operation: Operation): Context {
-  return effectionMain(({ context }) => {
-    let mainContext = context as any; // eslint-disable-line @typescript-eslint/no-explicit-any
+  return effectionMain(({ context: mainContext, spawn }) => {
 
-    mainContext.spawn(function*() {
+    spawn(function* main() {
+      let interrupt = () => { mainContext.halt(); };
       try {
+        process.on('SIGINT', interrupt);
         yield operation;
+        mainContext.halt();
       } catch(e) {
         console.error(e);
         process.exit(-1);
+      } finally {
+        process.off('SIGINT', interrupt);
       }
-    });
-
-    let interrupt = () => { mainContext.halt()};
-
-    process.on('SIGINT', interrupt);
-
-    mainContext.ensure(() => {
-      process.off('SIGINT', interrupt);
     });
   });
 }
