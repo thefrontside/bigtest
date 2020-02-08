63b38f1f1a6338fc70babd52874bbda9dc4d1ad6
Charles Lowell <cowboyd@frontside.io>
2020-02-08 11:30:42 -0600

    ♻Make Client a good ol' simple class
    The original implementation had a bunch of complexity to ensure that
    operations would be bound to the instance of the class. This just uses
    normal old class syntax. The only drawback is that would not be able
    to peel off a method and use it elsewhere:
    
    ```ts
    let { query } = client;
    yield query("{ agents { id }}"); // will not work!
    ```
    
    But if that becomes a requirement, we can use something
    like (autoBind)[1]
    
    [1]: https://github.com/sindresorhus/auto-bind

--- src/client.ts
+++ src/client.ts
@@ -1,89 +1,95 @@
 import { w3cwebsocket as WebSocket } from 'websocket';
 import { EventEmitter } from 'events';
-import { monitor, Operation } from 'effection';
+import { monitor, Operation, Context } from 'effection';
 import { on } from './effection/events'
 
 import { Message, isErrorResponse, isDataResponse } from './protocol';
 
-const parent: Operation = ({ resume, context: { parent }}) => resume(parent);
-
 export class Client {
-  query: (source: string) => Operation;
-  subscribe: (source: string, forEach: (result: unknown) => Operation) => Operation;
+  responseIds = 0;
+  subscriptions = new EventEmitter();
 
-  static *create(url: string): Operation {
-    let { parent: context } = yield parent;
-
-    let client = new Client();
-    let subscriptions = new EventEmitter();
-    let socket = new WebSocket(url);
-    let responseIds = 0;
-
+  private constructor(private socket: WebSocket, context: Context) {
+    let { subscriptions } = this;
     socket.onopen = () => subscriptions.emit('open');
     socket.onclose = event => subscriptions.emit('close', event);
     socket.onmessage = event => subscriptions.emit('message', event);
     socket.onerror = event => subscriptions.emit('error', event);
-
-    context.ensure(() => {
+    context['ensure'](() => {
       socket.onopen = socket.onclose = socket.onmessage = socket.onerror = null;
       socket.close();
     });
 
-    context.spawn(monitor(function* () {
+    context['spawn'](monitor(function* () {
       let [error]: [Error] = yield on(subscriptions, 'error');
       throw error;
     }));
 
-    context.spawn(monitor(function* () {
+    context['spawn'](monitor(function* () {
       yield on(subscriptions, 'close');
       throw new Error('Socket closed on the remote end');
     }));
+  }
 
-    function* send<Command>(command: Command, handle: (next: () => Operation) => Operation) {
-      let responseId = `${responseIds++}`; //we'd want a UUID to avoid hijacking?
-      let request: Message = {...command, responseId};
+  static *create(url: string): Operation {
+    let client = new Client(new WebSocket(url), yield parent);
 
-      socket.send(JSON.stringify(request));
-
-      let next = function* getNextResponse() {
-        while (true) {
-          let [event]: [MessageEvent] = yield on(subscriptions, 'message');
-          let message: Message = JSON.parse(event.data);
-
-          if (message.responseId === responseId) {
-            if (isErrorResponse(message)) {
-              let messages = message.errors.map(error => error.message);
-              throw new Error(messages.join("\n"));;
-            }
-            if (isDataResponse(message)) {
-              return message.data;
-            } else {
-              console.warn('unknown response format: ', event.data);
-            }
-          }
-        }
-      }
-
-      return yield handle(next);
-    }
-
-    client.query = function query(source: string): Operation {
-      return send({ query: source }, function*(next) {
-        return yield next();
-      });
-    }
-
-    client.subscribe = function subscribe(source, forEach): Operation {
-      return send({ query: source, live: true }, function*(next) {
-        while (true) {
-          let response = yield next();
-          yield forEach(response);
-        }
-      })
-    }
-
-    yield on(subscriptions, 'open');
+    yield on(client.subscriptions, 'open');
 
     return client;
   }
+
+  query(source: string): Operation {
+    return this.send({ query: source }, next => next());
+  }
+
+  subscribe(source: string, forEach: (response: unknown) => Operation): Operation {
+    return this.send({ query: source, live: true }, function*(next) {
+      while (true) {
+        let response = yield next();
+        yield forEach(response);
+      }
+    })
+  }
+
+  *send(command: Query, handle: HandleResponse): Operation {
+    let responseId = `${this.responseIds++}`; //we'd want a UUID to avoid hijacking?
+    let request: Message = {...command, responseId};
+
+    this.socket.send(JSON.stringify(request));
+
+    let { subscriptions } = this;
+
+    let next = function* getNextResponse() {
+      while (true) {
+        let [event]: [MessageEvent] = yield on(subscriptions, 'message');
+        let message: Message = JSON.parse(event.data);
+
+        if (message.responseId === responseId) {
+          if (isErrorResponse(message)) {
+            let messages = message.errors.map(error => error.message);
+            throw new Error(messages.join("\n"));;
+          }
+          if (isDataResponse(message)) {
+            return message.data;
+          } else {
+            console.warn('unknown response format: ', event.data);
+          }
+        }
+      }
+    }
+
+    return yield handle(next);
+  }
 }
+
+interface HandleResponse {
+  (next: () => Operation): Operation;
+}
+
+interface Query {
+  query: string;
+  live?: boolean;
+}
+
+const parent: Operation = ({ resume, context: { parent }}) => resume(parent.parent);
