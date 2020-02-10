d5f1b25f1a9b7069aaa3d6f4e7882b1f9c9c0384
Jonas Nicklas <jonas@jnicklas.com>
2020-02-10 18:14:10 +0100

    Use @effection/node for new binaries

--- bin/query.ts
+++ bin/query.ts
@@ -1,23 +1,13 @@
-import { main, Operation } from 'effection';
+import { Operation } from 'effection';
+import { main } from '@effection/node';
 import { Client } from '../src/client';
 
-const self: Operation = ({ resume, context: { parent }}) => resume(parent);
-
 main(function* main() {
-  let context = yield self;
-  let interrupt = () => { context.halt()};
-  process.on('SIGINT', interrupt);
-  try {
-    let client: Client = yield Client.create('ws://localhost:24002');
+  let client: Client = yield Client.create('ws://localhost:24002');
 
-    let [ source ]  = process.argv.slice(2);
+  let [ source ]  = process.argv.slice(2);
 
-    let data = yield client.query(source);
+  let data = yield client.query(source);
 
-    console.log(JSON.stringify(data, null, 2));
-  } catch (e) {
-    console.error(e);
-  } finally {
-    process.off('SIGINT', interrupt);
-  }
+  console.log(JSON.stringify(data, null, 2));
 })
--- bin/subscribe.ts
+++ bin/subscribe.ts
@@ -1,25 +1,14 @@
-import { main, Operation } from 'effection';
+import { Operation } from 'effection';
+import { main } from '@effection/node';
 import { Client } from '../src/client';
 
-const self: Operation = ({ resume, context: { parent }}) => resume(parent);
-
 main(function* main() {
-  let context = yield self;
-  let interrupt = () => { context.halt()};
-  process.on('SIGINT', interrupt);
-  try {
-    let client: Client = yield Client.create('ws://localhost:24002');
+  let client: Client = yield Client.create('ws://localhost:24002');
 
-    let [ source ]  = process.argv.slice(2);
+  let [ source ]  = process.argv.slice(2);
 
-    yield client.subscribe(source, function*(data) {
-      console.log('==== new subscription result ==== ');
-      console.log(JSON.stringify(data, null, 2));
-    });
-
-  } catch (e) {
-    console.error(e);
-  } finally {
-    process.off('SIGINT', interrupt);
-  }
+  yield client.subscribe(source, function*(data) {
+    console.log('==== new subscription result ==== ');
+    console.log(JSON.stringify(data, null, 2));
+  });
 })
