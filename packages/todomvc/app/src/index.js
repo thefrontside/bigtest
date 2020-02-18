f31ad9fafe815a583f813ae016abe13120d1c4f8
Charles Lowell <cowboyd@frontside.io>
2020-02-18 12:05:39 -0600

    Add TodoMVC server
    Motivation
    ----------
    
    Right now, we're hosting the react TodoMVC app inside bigtest
    itself which has a number of disadvantages:
    
    1. It takes a long time to boot up. In fact, most of the boot time of
       the orchestrator is waiting for the TodoMVC app to compile... even
       when we haven't changed its source once in over two months.
    2. It cannot be used outside of the actual server repo. This means
       that simulating an actual project is difficult since we really want
       the server, and the app to be running in their _own_ directory, and
       not out of the `@bigtest/server` repo.
    
    These problems will only become worse as we try to add apps from other
    frameworks. Instead, we'd like working with non-trivial example apps
    to be as effortless as possible.
    
    Approach
    --------
    
    Bundle the built TodoMVC app that we're currently using (based on
    create-react-app) as an NPM package that can be started either from
    the command line, or from code. This will allow us to more easily
    simulate real life projects that live in their own directories and
    have their own server code.
    
    By including `@bigtest/todomvc` in your dev dependencies, you can now
    start a server using the `bigtest-todomvc` command.

---
+++ packages/todomvc/app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {render} from 'react-dom'
+import App from './components/App'
+
+import 'todomvc-app-css/index.css'
+
+render(
+    <App />,document.getElementById('root')
+)
