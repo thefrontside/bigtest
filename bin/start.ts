52a3c0b7a77d303ab95a82975937c0a7ea3f36da
Charles Lowell <cowboyd@frontside.io>
2020-02-19 13:18:53 -0600

    Use @bigtest/agent

--- bin/start.ts
+++ bin/start.ts
@@ -24,6 +24,7 @@ main(function*() {
     commandPort: 24002,
     connectionPort: 24003,
     agentPort: 24004,
+    externalAgentServerURL: process.env['BIGTEST_AGENT_SERVER_URL'],
     testFilePort: 24005,
     testFiles: ["./test/fixtures/*.t.ts"],
     testManifestPath: tempy.file({ name: 'manifest.js' }),
