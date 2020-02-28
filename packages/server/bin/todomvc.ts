9d829e68fb7c94165a24ad80ef4ec713c0efe064
Charles Lowell <cowboyd@frontside.io>
2020-02-28 14:58:46 -0600

    Bring back the ability to start on a port

--- packages/server/bin/todomvc.ts
+++ packages/server/bin/todomvc.ts
@@ -2,10 +2,19 @@ import { TodoMVC } from '@bigtest/todomvc';
 import { main } from '@effection/node';
 
 main(function* start() {
-  let server: TodoMVC = yield TodoMVC.react(2400);
+  let server: TodoMVC = yield TodoMVC.react(port());
 
   console.info(`serving TodoMVC application`);
   console.info(`--> http://localhost:${server.port}`);
 
   yield server.join();
 });
+
+function port(): number | undefined {
+  let [,, second ] = process.argv;
+  if (second) {
+    return parseInt(second);
+  } else {
+    return undefined;
+  }
+}
