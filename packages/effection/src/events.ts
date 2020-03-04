e66c5d9b4114d7cbb5ae59f880e66e8f31eb4afa
Jonas Nicklas <jonas@jnicklas.com>
2020-03-04 17:26:58 +0100

    Just use string for `once`

--- packages/effection/src/events.ts
+++ packages/effection/src/events.ts
@@ -1,13 +1,11 @@
 import { Operation } from 'effection';
 import { EventEmitter } from 'events';
 
-type EventName = string | symbol;
-
 /**
  * Takes an event emitter and event name and returns a yieldable
  * operation which resumes when the event occurs.
  */
-export function once(emitter: EventEmitter, eventName: EventName): Operation {
+export function once(emitter: EventEmitter, eventName: string): Operation {
   return ({ resume, ensure }) => {
     let listener = (...args: unknown[]) => resume(args);
     ensure(() => emitter.off(eventName, listener));
