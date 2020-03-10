70e16081d41a534548ec3e393f98f853e994d3ba
Charles Lowell <cowboyd@frontside.io>
2020-03-10 13:16:10 -0500

    Integrate @bigtest/parcel into @bigtest/server

--- packages/server/src/manifest-builder.ts
+++ packages/server/src/manifest-builder.ts
@@ -1,6 +1,6 @@
 import { Operation } from 'effection';
 import { Mailbox } from '@bigtest/effection';
-import { ChildProcess, fork as forkProcess } from '@effection/child_process';
+import { ParcelProcess } from '@bigtest/parcel';
 import * as path from 'path';
 import * as fs from 'fs';
 import * as fprint from 'fprint';
@@ -42,27 +42,16 @@ function* processManifest(options: ManifestBuilderOptions): Operation {
 }
 
 export function* createManifestBuilder(options: ManifestBuilderOptions): Operation {
-  // TODO: @precompile this should use node rather than ts-node when running as a compiled package
-  let child: ChildProcess = yield forkProcess(
-    './bin/parcel-server.ts',
-    ['--out-dir', options.buildDir, '--out-file', 'manifest.js', '--global', '__bigtestManifest', options.srcPath],
-    {
-      execPath: 'ts-node',
-      execArgv: [],
-      stdio: ['pipe', 'pipe', 'pipe', 'ipc']
-    }
-  );
 
-  let messages = yield Mailbox.subscribe(child, "message", ({ args: [message] }) => message);
+  let parcel: ParcelProcess = yield ParcelProcess.create(options);
 
-  yield messages.receive({ type: "ready" });
   let distPath = yield processManifest(options);
 
   console.debug("[manifest builder] manifest ready");
   options.delegate.send({ status: "ready", path: distPath });
 
   while(true) {
-    yield messages.receive({ type: "update" });
+    yield parcel.receive({ type: "update" });
     let distPath = yield processManifest(options);
 
     console.debug("[manifest builder] manifest updated");
