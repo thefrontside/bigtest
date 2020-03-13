8a380774dcaa5c7d259175e35204589638e119ff
Charles Lowell <cowboyd@frontside.io>
2020-03-13 17:08:12 -0500

    Remove old TodoMVC app since it is now a separate package

--- packages/server/test/app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {render} from 'react-dom'
-import App from './components/App'
-
-import 'todomvc-app-css/index.css'
-
-render(
-    <App />,document.getElementById('root')
-)
