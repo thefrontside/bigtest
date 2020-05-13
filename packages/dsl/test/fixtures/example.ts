2a8c361df3946c57de562aa20faf9b3237b4e210
Jonas Nicklas <jonas@jnicklas.com>
2020-05-13 16:02:20 +0200

    Add test

--- packages/dsl/test/fixtures/example.ts
+++ packages/dsl/test/fixtures/example.ts
@@ -1,27 +1,26 @@
 import { test, assert } from '../../src/index';
 
-export default test("a test")
-  .step("some step", async () => {
-    return { foo: "foo" }
+export default test('a test')
+  .step('some step', async () => {
+    return { foo: 'foo' }
   })
-  .step("this does nothing", async() => {
-
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  .step('this does nothing', async() => {})
+  .step('another step', async ({ foo }) => {
+    return { bar: foo.toUpperCase() + 'bar' }
   })
-  .step("another step", async ({ foo }) => {
-    return { bar: foo.toUpperCase() + "bar" }
+  .assertion('this is an assertion', ({ foo }) => {
+    assert.equal(foo, 'foo');
   })
-  .assertion("this is an assertion", ({ foo }) => {
-    assert.equal(foo, "foo");
+  .assertion('this is another assertion', ({ bar }) => {
+    assert.equal(bar, 'foobar');
   })
-  .assertion("this is another assertion", ({ bar }) => {
-    assert.equal(bar, "foobar");
-  })
-  .child("a child test", (test) => (
+  .child('a child test', (test) => (
     test
-      .step("a child step", async ({ foo }) => {
-        return { quox: foo.toUpperCase() + "blah" }
+      .step('a child step', async ({ foo }) => {
+        return { quox: foo.toUpperCase() + 'blah' }
       })
-      .assertion("a child assertion", ({ quox }) => {
-        assert.equal(quox, "FOOblah");
+      .assertion('a child assertion', ({ quox }) => {
+        assert.equal(quox, 'FOOblah');
       })
   ))
