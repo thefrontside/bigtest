528029c0904a704843238d0b80f60938d642de8c
Jonas Nicklas <jonas@jnicklas.com>
2020-05-13 14:44:50 +0200

    simplify

--- packages/dsl/src/index.ts
+++ packages/dsl/src/index.ts
@@ -24,39 +24,30 @@ class TestBuilder<C extends Context> implements TestImplementation {
   }
 
   step<R extends Context | void>(description: string, action: (context: C) => PromiseLike<R>): TestBuilder<R extends void ? C : C & R> {
-    let implementation = {
-      description: this.description,
+    return new TestBuilder({
+      ...this,
       steps: this.steps.concat({
         description,
         action: action as Action
       }),
-      assertions: this.assertions,
-      children: this.children
-    }
-    return new TestBuilder(implementation);
+    });
   }
 
   assertion<R extends Context | void>(description: string, check: (context: C) => R): TestBuilder<R extends void ? C : C & R> {
-    let implementation = {
-      description: this.description,
-      steps: this.steps,
+    return new TestBuilder({
+      ...this,
       assertions: this.assertions.concat({
         description,
         check: check as Check
       }),
-      children: this.children
-    }
-    return new TestBuilder(implementation);
+    });
   }
 
   child(description: string, childFn: (inner: TestBuilder<C>) => TestBuilder<Context>): TestBuilder<C> {
     let child = childFn(test(description));
-    let implementation = {
-      description: this.description,
-      steps: this.steps,
-      assertions: this.assertions,
+    return new TestBuilder({
+      ...this,
       children: this.children.concat(child)
-    }
-    return new TestBuilder(implementation);
+    });
   }
 }
