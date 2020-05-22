93252d4d263fe86cb83ee5cf941ce11a00fa9f75
Jonas Nicklas <jonas@jnicklas.com>
2020-05-22 12:37:04 +0200

    Add `exists` function

---
+++ packages/interactor/src/index.ts
@@ -0,0 +1,74 @@
+import { main, timeout } from 'effection';
+
+// TODO: this API is available on browsers as `window.performance`, we need to figure out
+// a way to package this so it'll work on both browsers and node.
+import { performance } from 'perf_hooks';
+
+interface Options {
+  timeout: number,
+  document?: HTMLDocument
+}
+
+let defaultOptions: Options = {
+  timeout: 1900
+}
+
+async function converge<T>(timeout: number, fn: () => T): Promise<T> {
+  return await main(function*() {
+    let startTime = performance.now();
+    while(true) {
+      try {
+        return fn();
+      } catch(e) {
+        let diff = performance.now() - startTime;
+        if(diff > timeout) {
+          throw e;
+        }
+      }
+    }
+  });
+}
+
+export interface InteractorSpecification {
+  name: string,
+  selector: string,
+  defaultLocator: (element: HTMLElement) => string;
+}
+
+export type LocatorSpecification = string;
+
+export class Interactor {
+  constructor(private specification: InteractorSpecification, private locator: LocatorSpecification) {
+  }
+
+  exists(): Promise<true> {
+    return converge(defaultOptions.timeout, () => {
+      if(!defaultOptions.document) {
+        throw new Error('must specify document');
+      }
+      let elements = defaultOptions.document.querySelectorAll(this.specification.selector);
+
+      let matchingElements = [].filter.call(elements, (element) => {
+        return this.specification.defaultLocator(element) == this.locator
+      });
+
+      if(matchingElements.length === 1) {
+        return true;
+      } else if(matchingElements.length === 0) {
+        throw new Error(`${this.specification.name} ${JSON.stringify(this.locator)} does not exist`);
+      } else {
+        throw new Error(`${this.specification.name} ${JSON.stringify(this.locator)} is ambiguous`);
+      }
+    });
+  }
+}
+
+export function interactor(specification: InteractorSpecification): (locator: LocatorSpecification) => Interactor {
+  return function(locator: LocatorSpecification) {
+    return new Interactor(specification, locator);
+  }
+}
+
+export function setDefaultOptions(options: Partial<Options>) {
+  Object.assign(defaultOptions, options);
+}
--- packages/interactor/test/interactor.test.ts
+++ packages/interactor/test/interactor.test.ts
@@ -1,8 +1,49 @@
 import { describe, it } from 'mocha';
 import * as expect from 'expect'
+import { JSDOM } from 'jsdom';
 
-describe("@bigtest/interactor", () => {
-  it('works', () => {
-    expect(1).toEqual(1);
+import { interactor, setDefaultOptions } from '../src/index';
+
+process.on('unhandledRejection', () => {
+  // do nothing
+});
+
+const Link = interactor({
+  name: 'link',
+  selector: 'a',
+  defaultLocator: (element) => element.innerText
+});
+
+function dom(html: string) {
+  let jsdom = new JSDOM(html);
+  setDefaultOptions({
+    document: jsdom.window.document,
+    timeout: 20,
+  });
+}
+
+describe('@bigtest/interactor', () => {
+  describe('.exists', () => {
+    it('can determine whether an element exists based on the interactor', () => {
+      dom(`
+        <p><a href="/foobar">Foo Bar</a></p>
+      `);
+
+      expect(Link('Foo Bar').exists()).resolves.toEqual(true);
+      expect(Link('Blah').exists()).rejects.toHaveProperty('message', 'link "Blah" does not exist');
+    });
+
+    it('can wait for condition to become true', () => {
+      dom(`
+        <p id="foo"></p>
+        <script>
+          setTimeout(() => {
+            foo.innerHTML = '<a href="/foobar">Foo Bar</a>';
+          }, 5);
+        </script>
+      `);
+
+      expect(Link('Foo Bar').exists()).resolves.toEqual(true);
+    });
   });
 })
