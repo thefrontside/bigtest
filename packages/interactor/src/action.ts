6fe209e44f9c6f78819116791b2aaad93cfcb804
Jonas Nicklas <jonas@jnicklas.com>
2020-05-23 15:07:26 +0200

    Add generic type for element on interactor specification

--- packages/interactor/src/action.ts
+++ packages/interactor/src/action.ts
@@ -1,5 +1,5 @@
-export type ActionSpecification = Record<string, (element: HTMLElement) => unknown>
+export type ActionSpecification<E extends HTMLElement> = Record<string, (element: E) => unknown>
 
-export type ActionImplementation<T extends ActionSpecification> = {
-  [P in keyof T]: T[P] extends ((element: HTMLElement, ...args: infer TArgs) => infer TReturn) ? ((...args: TArgs) => Promise<TReturn>) : never;
+export type ActionImplementation<E extends HTMLElement, T extends ActionSpecification<E>> = {
+  [P in keyof T]: T[P] extends ((element: E, ...args: infer TArgs) => infer TReturn) ? ((...args: TArgs) => Promise<TReturn>) : never;
 }
