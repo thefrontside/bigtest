2993dccc2d56e4a7934f4a1cf4f3c0977a0c73fa
Jonas Nicklas <jonas@jnicklas.com>
2020-05-27 21:05:04 +0200

    Fix composite interactors by storing entire ancestor chain
    Each interactor will store the entire chain of its ancestors, thereby avoiding problems when composing interactors together.

--- packages/interactor/src/action.ts
+++ packages/interactor/src/action.ts
@@ -1,5 +1,5 @@
-export type ActionSpecification<E extends HTMLElement> = Record<string, (element: E) => unknown>
+export type ActionSpecification<E extends Element> = Record<string, (element: E) => unknown>
 
-export type ActionImplementation<E extends HTMLElement, T extends ActionSpecification<E>> = {
+export type ActionImplementation<E extends Element, T extends ActionSpecification<E>> = {
   [P in keyof T]: T[P] extends ((element: E, ...args: infer TArgs) => infer TReturn) ? ((...args: TArgs) => Promise<TReturn>) : never;
 }
