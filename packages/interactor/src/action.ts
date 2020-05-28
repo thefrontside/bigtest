97772e7e25c3c7a251e85a1847c41e31c8d38b78
Jonas Nicklas <jonas@jnicklas.com>
2020-05-28 11:10:29 +0200

    Make interactions lazy and add description
    We're intending for these lazy interactions to be used in conjunction with the test suite DSL, so we can write things like:
    
    ```
    .step(Link("Next").click())
    ```
    
    Since the interaction won't do anything until awaited, we can use this to create a step which infers its own description and doesn't need an anonymous function, so it makes for a more concise and user-friendly API.

--- packages/interactor/src/action.ts
+++ packages/interactor/src/action.ts
@@ -1,5 +1,7 @@
+import { Interaction } from './interaction';
+
 export type ActionSpecification<E extends Element> = Record<string, (element: E) => unknown>
 
 export type ActionImplementation<E extends Element, T extends ActionSpecification<E>> = {
-  [P in keyof T]: T[P] extends ((element: E, ...args: infer TArgs) => infer TReturn) ? ((...args: TArgs) => Promise<TReturn>) : never;
+  [P in keyof T]: T[P] extends ((element: E, ...args: infer TArgs) => infer TReturn) ? ((...args: TArgs) => Interaction<TReturn>) : never;
 }
