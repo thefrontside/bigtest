201968d009f8c93293a099e0bd21c89aaeece7b7
Charles Lowell <cowboyd@frontside.io>
2020-06-10 15:26:30 -0500

    make node driver connect

--- packages/nodedriver/index.ts
+++ packages/nodedriver/index.ts
@@ -1,8 +1,8 @@
 import { Operation, resource, spawn, contextOf, Context } from 'effection';
-import { throwOnErrorEvent } from '@effection/events';
-import { Subscribable } from '@effection/subscription';
+import { throwOnErrorEvent, once } from '@effection/events';
+import { Subscribable, SymbolSubscribable, createSubscription, forEach } from '@effection/subscription';
 import { DriverSpec, Driver } from '@bigtest/driver';
-import { AgentProtocol, Command } from '@bigtest/agent';
+import { Command, AgentEvent } from '@bigtest/agent';
 
 import * as WebSocket from 'ws';
 
@@ -33,15 +33,67 @@ class NodeDriver {
     }
 
     this.context.spawn(function*() {
-      yield Connection.create(connectTo);
-      let socket = new WebSocket(connectTo);
+      let websocket = new WebSocket(connectTo);
+      yield throwOnErrorEvent(websocket);
 
+      yield once(websocket, "open");
+
+      try {
+        let connection = createConnection(websocket, agentId);
+
+        yield forEach(connection, function*(command) {
+          console.log('command =', command);
+        });
+      } finally {
+        websocket.close();
+      }
     })
   }
 }
 
-class Connection implements AgentProtocol, Subscribable<Command, void> {
+class Connection implements Subscribable<Command, void> {
+  constructor(private socket: WebSocket) {}
 
+  send(event: AgentEvent) {
+    this.socket.send(JSON.stringify(event));
+  }
+
+  [SymbolSubscribable]() {
+    let { socket } = this;
+    return createSubscription<Command, void>(function*(publish) {
+      spawn(function*() {
+        try {
+          socket.on('message', publish)
+          yield;
+        } finally {
+          socket.off('message', publish);
+        }
+      });
+
+      yield once(socket, 'close');
+    });
+  }
+}
+
+function createConnection(socket: WebSocket, agentId: string) {
+  let connection = new Connection(socket);
+  let os = require('os');
+  connection.send({
+    type: "connected",
+    agentId,
+    data: {
+      os: {
+        name: os.type(),
+        version: os.release(),
+        versionName: os.platform()
+      },
+      platform: {
+        type: os.arch(),
+        vendor: os.cpus()[0].model
+      }
+    }
+  });
+  return connection;
 }
 
 interface SpawnPoint {
