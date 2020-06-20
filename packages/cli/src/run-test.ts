e5657d44231a8e06276f5ef5b89ccc84bc15f370
Jonas Nicklas <jonas@jnicklas.com>
2020-06-20 14:16:48 +0200

    Add streaming output of test results

--- packages/cli/src/run-test.ts
+++ packages/cli/src/run-test.ts
@@ -2,24 +2,53 @@ import { ProjectOptions } from '@bigtest/project';
 import { Client } from '@bigtest/server';
 import * as query from './query';
 
+function icon(event: query.RunResultEvent) {
+  if(event.type.match(/^:running$/)) {
+    return "↻";
+  } else if(event.status === 'ok') {
+    return "✓";
+  } else if(event.status === 'failed') {
+    return "⨯";
+  } else if(event.status === 'disregarded') {
+    return "⋯";
+  }
+}
+
+function filterStack(text: string): string {
+  return text.split('\n').filter((l) => !l.match(/__bigtest/)).join('\n');
+}
+
+function prefixLines(text: string, prefix: string) {
+  return text
+    .split('\n')
+    .map((l) => prefix + l)
+    .join('\n')
+}
+
+function formatEvent(event: query.RunResultEvent) {
+  let result = `${icon(event)} [${event.type.split(':')[0]}]`.padEnd(14);
+  if(event.path) {
+    result += ' ' + event.path.slice(1).join(' -> ');
+  }
+  if(event.error) {
+    result += '\n' + prefixLines(event.error.stack ? filterStack(event.error.stack) : `Error: ${event.error.message}`, '|   ')
+  }
+  return result;
+}
+
 export function* runTest(config: ProjectOptions) {
   let client: Client = yield Client.create(`ws://localhost:${config.port}`);
 
-  let { run: testRunId } = yield client.query(query.run());
-
-  console.log('Starting test run:', testRunId);
-
-  let subscription = yield client.liveQuery(query.testRunResults(testRunId));
+  let subscription = yield client.subscription(query.run());
 
   while(true) {
-    let { testRun } = yield subscription.receive();
-    if(testRun.status === 'ok') {
-      console.log('SUCCESS');
-      break;
-    }
-    if(testRun.status === 'failed') {
-      console.log('FAILED');
-      break;
+    let result: query.RunResult = yield subscription.receive();
+    if(query.isDoneResult(result)) {
+      return;
+    } else if(result.event) {
+      if(result.event.type === 'step:result' || result.event.type === 'assertion:result') {
+        console.log(formatEvent(result.event))
+      }
     }
   }
 }
