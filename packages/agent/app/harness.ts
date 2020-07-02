ea5784a89a48218f2c9046318b9bebcb78ced992
Jonas Nicklas <jonas@jnicklas.com>
2020-07-02 18:27:10 +0200

    Proxy fetch and XMLHttpRequest through agent frame
    This makes it possible to use mocking libraries like mirage from the test suite. By mocking XMLHttpRequest in the agent, we're also automatically using those mocks in the app/harness.

--- packages/agent/app/harness.ts
+++ packages/agent/app/harness.ts
@@ -4,3 +4,13 @@ import { createHarness } from './create-harness';
 
 main(createHarness())
   .catch(error => console.error(error));
+
+
+// proxy fetch requests through the parent frame
+window.fetch = (input: RequestInfo, init?: RequestInit): Promise<Response> => {
+  return window.parent.window.fetch(input, init);
+}
+
+// proxy XMLHttpRequest through the parent frame
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(window as any).XMLHttpRequest = () => new window.parent.window.XMLHttpRequest();
