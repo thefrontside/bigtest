88c4d9927ffc92bb44ac01a5f516ed66223cfaa7
Jonas Nicklas <jonas@jnicklas.com>
2020-07-03 16:48:49 +0200

    Only proxy requests if running inside iframe

--- packages/agent/app/harness.ts
+++ packages/agent/app/harness.ts
@@ -14,11 +14,12 @@ main(function*() {
 }).catch(error => console.error(error));
 
 
-// proxy fetch requests through the parent frame
-window.fetch = (input: RequestInfo, init?: RequestInit): Promise<Response> => {
-  return window.parent.window.fetch(input, init);
-}
+// proxy fetch and XMLHttpRequest requests through the parent frame
+if(window.parent !== window) {
+  window.fetch = (input: RequestInfo, init?: RequestInit): Promise<Response> => {
+    return window.parent.window.fetch(input, init);
+  }
 
-// proxy XMLHttpRequest through the parent frame
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-(window as any).XMLHttpRequest = () => new window.parent.window.XMLHttpRequest();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (window as any).XMLHttpRequest = () => new window.parent.window.XMLHttpRequest();
+}
