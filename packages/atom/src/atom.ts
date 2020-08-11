add616006808cd009c2b25bacc8c9273a09fb167
Paul Cowan <paul.cowan@disclosurescotland.gov.scot>
2020-08-11 23:35:06 +0100

    add slice overloads into a generic type

--- packages/atom/src/atom.ts
+++ packages/atom/src/atom.ts
@@ -2,6 +2,7 @@ import { Operation } from "effection";
 import { Channel } from '@effection/channel';
 import { subscribe, Subscribable, SymbolSubscribable, Subscription } from '@effection/subscription';
 import { Slice } from "./slice";
+import { Sliceable } from './sliceable';
 
 export class Atom<S> implements Subscribable<S,undefined> {
   private readonly initial: S;
@@ -38,39 +39,8 @@ export class Atom<S> implements Subscribable<S,undefined> {
     }
   }
 
-
-  /* 
-  * Brute foce overload to allow strong typing of the string path syntax
-  * for every level deep we want to go in the slice there will need to be an overload 
-  * with an argument for each of the string paths
-  * 
-  * e.g. atom.slice('agents', agentId);  // 2 levels deep
-  * 
-  * There must be a matching overload
-  * 
-  * slice<Key1 extends keyof S, Key2 extends keyof S[Key1]>(key1: Key1, key2: Key2): Slice<S[Key1][Key2], S>;
-  * 
-  * key1 is 'agents' and key2 is 'agents'.
-  * 
-  * key1 is constrained to be a key of the atom `agents` which is atom atom.agents or could be
-  * atom.manifest or atom.testRuns of Atom<OrchestratorState>
-  * 
-  * key2 is constrained to be a key of the return type S[Key1] which is the agents object and in this
-  * example whatever the agentId variable is
-  * 
-  * The return type of the function is Slice<S[Key1][Key2], S>; or Slice<AgentState, OrchestratorState>
-  * in this example
-  */
-  slice<Key extends keyof S>(key: Key): Slice<S[Key], S>;
-  slice<Key1 extends keyof S, Key2 extends keyof S[Key1]>(key1: Key1, key2: Key2): Slice<S[Key1][Key2], S>;
-  slice<Key1 extends keyof S, Key2 extends keyof S[Key1], Key3 extends keyof S[Key1][Key2]>(key1: Key1, key2: Key2, key3: Key3): Slice<S[Key1][Key2][Key3], S>;
-  slice<Key1 extends keyof S, Key2 extends keyof S[Key1], Key3 extends keyof S[Key1][Key2], Key4 extends keyof S[Key1][Key2][Key3]>(key1: Key1, key2: Key2, key3: Key3, key4: Key4): Slice<S[Key1][Key2][Key3][Key4], S>;
-  slice<Key1 extends keyof S, Key2 extends keyof S[Key1], Key3 extends keyof S[Key1][Key2], Key4 extends keyof S[Key1][Key2][Key3], Key5 extends keyof S[Key1][Key2][Key3][Key4]>(key1: Key1, key2: Key2, key3: Key3, key4: Key4, key5: Key5): Slice<S[Key1][Key2][Key3][Key4][Key5], S>;
-  slice<Key1 extends keyof S, Key2 extends keyof S[Key1], Key3 extends keyof S[Key1][Key2], Key4 extends keyof S[Key1][Key2][Key3], Key5 extends keyof S[Key1][Key2][Key3][Key4], Key6 extends keyof S[Key1][Key2][Key3][Key4][Key5]>(key1: Key1, key2: Key2, key3: Key3, key4: Key4, key5: Key5, key6: Key6): Slice<S[Key1][Key2][Key3][Key4][Key5][Key6], S>;
-  slice<Key1 extends keyof S, Key2 extends keyof S[Key1], Key3 extends keyof S[Key1][Key2], Key4 extends keyof S[Key1][Key2][Key3], Key5 extends keyof S[Key1][Key2][Key3][Key4], Key6 extends keyof S[Key1][Key2][Key3][Key4][Key5], Key7 extends keyof S[Key1][Key2][Key3][Key4][Key5][Key6]>(key1: Key1, key2: Key2, key3: Key3, key4: Key4, key5: Key5, key6: Key6, key7: Key7): Slice<S[Key1][Key2][Key3][Key4][Key5][Key6][Key7], S>;
-  slice<Key1 extends keyof S, Key2 extends keyof S[Key1], Key3 extends keyof S[Key1][Key2], Key4 extends keyof S[Key1][Key2][Key3], Key5 extends keyof S[Key1][Key2][Key3][Key4], Key6 extends keyof S[Key1][Key2][Key3][Key4][Key5], Key7 extends keyof S[Key1][Key2][Key3][Key4][Key5][Key6], Key8 extends keyof S[Key1][Key2][Key3][Key4][Key5][Key6][Key7]>(key1: Key1, key2: Key2, key3: Key3, key4: Key4, key5: Key5, key6: Key6, key7: Key7, key8: Key8): Slice<S[Key1][Key2][Key3][Key4][Key5][Key6][Key7][Key8], S>;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  slice(...keys: string[]): Slice<any, S> {
+  slice: Sliceable<S, S>['slice'] = (...keys: string[]): Slice<any, S> => {
     return new Slice(this, keys);
   }
 
