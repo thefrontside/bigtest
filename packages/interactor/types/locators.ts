b21ca9ea58332c4ae5d52ea90fa34d541834ce2f
Robbie Pitts <me@robbie-pitts.com>
2020-08-14 10:32:02 -0400

    Add more Interactor types tests for locators

---
+++ packages/interactor/types/locators.ts
@@ -0,0 +1,39 @@
+import { createInteractor } from '../src/index';
+
+const Link = createInteractor<HTMLLinkElement>('link')({
+  selector: 'a',
+  defaultLocator: ['byHref', 'byTitle'],
+  locators: {
+    byHref: (element) => element.href,
+    byTitle: (element) => element.title
+  }
+});
+
+Link('foobar');
+
+Link.byHref('foobar');
+
+// cannot use wrong type argument on locator
+// $ExpectError
+Link.byHref(123);
+
+// cannot use locator which is not defined
+// $ExpectError
+Link.byValue('foobar');
+
+const Button = createInteractor<HTMLButtonElement>('button')({
+  selector: 'button',
+  defaultLocator: 'byText',
+  locators: {
+    byText: (element) => element.textContent || ""
+  }
+});
+
+Button('foobar');
+
+const TextField = createInteractor<HTMLInputElement>('text field')({
+  selector: 'input[type="text"]',
+  defaultLocator: (element) => element.placeholder,
+});
+
+TextField('foobar');
