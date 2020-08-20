50a97a9d59e1644345e3fa6955d0ba96c84bbd0d
Charles Lowell <cowboyd@frontside.io>
2020-08-20 16:37:50 -0500

    Consume websockets on the server using @effection/subscription
    In order to write tests for the bigtest agent, we need to simulate an
    orchestrator agent handler that the agent can connect to. However,
    the @bigtest/effection-express package requires you to write your
    websocket handler with the deprecated mailbox handler interface.
    
    Rather than replace the mailbox interface, this adds the subscription
    interface along side. That way, consumers of effection-express can
    migrate over gradually over time.
    
    In order to verify both interfaces, tests have been wrapped around the
    websocket API for both the Mailbox interface as well as the
    subscription interface.

---
+++ packages/effection-express/test/websocket.test.ts
@@ -0,0 +1,85 @@
+import { describe, beforeEach, it } from 'mocha';
+import * as expect from 'expect';
+
+import { timeout } from 'effection';
+import { Channel } from '@effection/channel';
+import { subscribe, ChainableSubscription } from '@effection/subscription';
+import * as WebSocket from 'ws';
+
+import { run } from './helpers';
+import { Socket, express } from '../src';
+import { Mailbox } from '@bigtest/effection/dist';
+
+
+describe.only('websocket server', () => {
+  let client: WebSocket;
+  let connection: Socket;
+
+  beforeEach(async () => {
+    let incoming = new Channel<Socket>();
+    let sockets = await run(subscribe(incoming));
+
+    let app = express();
+    await run(app.ws('*', function*(socket) {
+      incoming.send(socket);
+    }));
+    await run(app.listen(3400));
+
+
+    client = new WebSocket('http://127.0.0.1:3400');
+
+    connection = await run(sockets.expect());
+  });
+
+  it('accepts connections', () => {
+    expect(connection).toBeDefined();
+  });
+
+  describe('when receiving messages with the depraced mailbox API', () => {
+    let messages: Mailbox;
+
+    beforeEach(async () => {
+      messages = await run(connection.subscribe());
+
+      // we can't send messages until the client is connected
+      await run(function*() {
+        while (client.readyState !== client.OPEN) {
+          expect(client.readyState).toEqual(client.CONNECTING);
+          yield timeout(10);
+        }
+      })
+
+      client.send(JSON.stringify({ message: "Hello World!" }));
+      client.send(JSON.stringify({ message: "Goodbye World!" }));
+    });
+
+    it('publishes them on the server', async () => {
+      expect(await run(messages.receive())).toEqual({ message: "Hello World!" });
+      expect(await run(messages.receive())).toEqual({ message: "Goodbye World!" });
+    });
+  });
+
+  describe('when receiving messages via subscription', () => {
+    let messages: ChainableSubscription<unknown, void>;
+
+    beforeEach(async () => {
+      messages = await run(subscribe(connection));
+
+      // we can't send messages until the client is connected
+      await run(function*() {
+        while (client.readyState !== client.OPEN) {
+          expect(client.readyState).toEqual(client.CONNECTING);
+          yield timeout(10);
+        }
+      })
+
+      client.send(JSON.stringify({ message: "Hello World!" }));
+      client.send(JSON.stringify({ message: "Goodbye World!" }));
+    });
+
+    it('publishes them on the server', async () => {
+      expect(await run(messages.expect())).toEqual({ message: "Hello World!" });
+      expect(await run(messages.expect())).toEqual({ message: "Goodbye World!" });
+    });
+  });
+})
