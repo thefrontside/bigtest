54c053fffa23a8bc3453df4213d825aad786d0ee
Paul Cowan <paul.cowan@disclosurescotland.gov.scot>
2020-09-06 16:57:45 +0100

    remove spurious console.log in logger

--- packages/server/src/logger.ts
+++ packages/server/src/logger.ts
@@ -11,7 +11,6 @@ export function* createLogger({ atom, out }: LoggerOptions) {
   let bundlerState = atom.slice('bundler');
 
   yield subscribe(bundlerState).forEach(function* (event) {
-    console.log(event);
     if(event.type === 'ERRORED'){
       out("[manifest builder] build error:", event.error);
       if (event.error.frame) {
