e3eb40b125231f664c0dad759eb8240e0719f123
Jonas Nicklas <jonas@jnicklas.com>
2020-09-07 16:03:21 +0200

    Clean up JSDOM after each test

--- packages/interactor/test/helpers.ts
+++ packages/interactor/test/helpers.ts
@@ -2,8 +2,10 @@ import { beforeEach } from 'mocha';
 import { bigtestGlobals } from '@bigtest/globals';
 import { JSDOM } from 'jsdom';
 
+let jsdom: JSDOM;
+
 export function dom(html: string) {
-  let jsdom = new JSDOM(`<!doctype html><html><body>${html}</body></html>`, { runScripts: "dangerously" });
+  jsdom = new JSDOM(`<!doctype html><html><body>${html}</body></html>`, { runScripts: "dangerously" });
   bigtestGlobals.document = jsdom.window.document;
 }
 
@@ -12,3 +14,6 @@ beforeEach(() => {
   bigtestGlobals.defaultInteractorTimeout = 20;
 });
 
+afterEach(() => {
+  jsdom?.window?.close();
+});
