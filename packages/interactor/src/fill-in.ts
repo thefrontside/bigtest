434e44c374e67c4df398ca2058979a4ed940f952
Jonas Nicklas <jonas@jnicklas.com>
2020-09-29 14:22:20 +0200

    Restructure `fillIn`

--- packages/interactor/src/fill-in.ts
+++ packages/interactor/src/fill-in.ts
@@ -8,16 +8,17 @@ function guessCode(letter: string): string | undefined {
   }
 }
 
-export function fillIn(element: HTMLInputElement, value: string) {
-  let Event = element.ownerDocument.defaultView?.Event || window.Event;
-  let InputEvent = element.ownerDocument.defaultView?.InputEvent || window.InputEvent;
-  let KeyboardEvent = element.ownerDocument.defaultView?.KeyboardEvent || window.KeyboardEvent;
-
-  element.focus();
+function clearText(element: HTMLInputElement) {
   if(element.value.length) {
     element.value = ""
     element.dispatchEvent(new InputEvent('input', { bubbles: true, cancelable: false, inputType: 'deleteContentBackward', data: null }));
   }
+}
+
+function enterText(element: HTMLInputElement, value: string) {
+  let InputEvent = element.ownerDocument.defaultView?.InputEvent || window.InputEvent;
+  let KeyboardEvent = element.ownerDocument.defaultView?.KeyboardEvent || window.KeyboardEvent;
+
   for(let letter of value) {
     let keydownEvent = new KeyboardEvent('keydown', { bubbles: true, cancelable: true, key: letter, code: guessCode(letter) });
     if(element.dispatchEvent(keydownEvent)) {
@@ -29,6 +30,17 @@ export function fillIn(element: HTMLInputElement, value: string) {
     // keyup is always dispatched
     element.dispatchEvent(new KeyboardEvent('keyup', { bubbles: true, cancelable: true, key: letter, code: guessCode(letter) }));
   }
+}
+
+export function fillIn(element: HTMLInputElement, value: string) {
+  let Event = element.ownerDocument.defaultView?.Event || window.Event;
+
+  element.focus();
+
+  clearText(element);
+  enterText(element, value);
+
   element.dispatchEvent(new Event('change', { bubbles: true, cancelable: false }));
+
   element.blur();
 }
