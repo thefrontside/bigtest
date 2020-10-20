5d7f9043715caa80b83454cc6c74dc95dd881283
Charles Lowell <cowboyd@frontside.io>
2020-10-20 06:38:15 -0500

    directly destructure close event with no intermediate

--- packages/server/src/connection-server.ts
+++ packages/server/src/connection-server.ts
@@ -35,12 +35,11 @@ export function* createConnectionServer(options: ConnectionServerOptions): Opera
         }
       });
 
-      let close: CloseEvent = yield subscribe(connection.events).forEach(function*(message: TestEvent) {
+      let { code, reason }: CloseEvent = yield subscribe(connection.events).forEach(function*(message: TestEvent) {
         console.debug('[connection] got message from agent', connection.agentId, message);
         options.delegate.send({ ...message, agentId: connection.agentId });
       });
 
-      let { code, reason } = close;
       console.debug(`[connection] disconnected ${connection.agentId} [${code}${reason ? `: ${reason}` : ''}]`);
 
       agent.remove();
