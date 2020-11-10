1a4c29ab4fda973aec062fa80cb67cb4cb4f1642
Paul Cowan <paul.cowan@disclosurescotland.gov.scot>
2020-11-10 18:23:08 +0000

    refactor replace

--- packages/atom/src/slice.ts
+++ packages/atom/src/slice.ts
@@ -1,25 +1,23 @@
 import { Operation } from "effection";
 import { Lens } from "monocle-ts";
-import * as L from "monocle-ts/lib/Lens"
-
+import * as O from 'fp-ts/Option';
 import { subscribe, Subscribable, SymbolSubscribable, Subscription } from '@effection/subscription';
 import { Sliceable } from './sliceable';
 import { assert } from 'assert-ts';
 import { Atom } from "./atom";
-import { pipe } from "fp-ts/lib/function";
-import * as RR from "fp-ts/lib/ReadonlyRecord"
+import { atRecord } from 'monocle-ts/lib/At/Record';
+
 
 export class Slice<S, A> implements Subscribable<S, void> {
   private constructor(private atom: Atom<A>, private lens: Lens<A, S>, private path: (keyof A)[]) {}
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  static fromPath<S, A>(a: Atom<S>): Sliceable<S, A> {
-    return <P extends keyof S>(...path: [P]) => {
-      let lens = Lens.fromPath<S>()(path);
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      return new Slice(a, lens, path) as any;
-    }
+static fromPath<S, A>(a: Atom<S>): Sliceable<S, A> {
+  return <P extends keyof S>(...path: [P]) => {
+    let lens = Lens.fromPath<S>()(path);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return new Slice(a, lens, path) as any;
   }
+}
 
   get state() {
     return this.atom.get();
@@ -47,13 +45,16 @@ export class Slice<S, A> implements Subscribable<S, void> {
     assert(this.path.length > 1, "invalid path in Slice#remove");
 
     let parentPath = this.path.slice(0, -1);
-    let prop = this.path.slice(-1)[0];
+    let prop = this.path.slice(-1)[0] as string;
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     let parentLens = Lens.fromPath<any>()(parentPath as [keyof A]);
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    this.atom.set(parentLens.modify(RR.deleteAt(prop as any))(this.state));
+    let current = parentLens.compose(atRecord<S>().at(prop as string));
+
+    let nextState = current.set(O.none)(this.state);
+
+    this.atom.set(nextState);
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -62,7 +63,7 @@ export class Slice<S, A> implements Subscribable<S, void> {
 
     let fromPath = Lens.fromPath<S>()(path);
 
-    return new Slice(this.atom, this.lens.composeLens(fromPath), [...this.path, ...path] as unknown as (keyof A)[]);
+    return new Slice(this.atom, this.lens.composeLens(fromPath), [...this.path, ...path] as (keyof A)[]);
   }
 
   *once(predicate: (state: S) => boolean): Operation<S> {
