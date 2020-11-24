1cf838815258a43233b0f28dd73727a5080685db
minkimcello <minkimcello@gmail.com>
2020-11-24 12:43:18 -0500

    Remove why bigtest and move comments to interactor landing page

--- website/docs/src/pages/interactors.js
+++ website/docs/src/pages/interactors.js
@@ -6,6 +6,22 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
 
+/* 
+
+// originally from interactors/getting-started/why-bigtest
+
+Who should use BigTest? (need a better name for this) - someone should be able to link to this page, and it is their persuasive argument for why the team should adopt these tools. These are not sections but more like points that should be covered in some way:
+
+1-2 sentences that explain the “why”
+
+What can you test with Interactors/BigTest?
+
+What problems does this solve/benefits? (Easier to write tests, refactor UIs, shared testing helpers for component libraries, can test for a11y, etc)
+
+How this fits into an existing testing strategy 
+
+*/
+
 function Interactors() {
   let context = useDocusaurusContext();
   let {siteConfig = {}} = context;
