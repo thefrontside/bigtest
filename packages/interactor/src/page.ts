4540c9e340a88d25d58bf7b38afd2c7e7d73cab2
Jonas Nicklas <jonas@jnicklas.com>
2020-12-02 12:48:44 +0100

    Document `Page`

--- packages/interactor/src/page.ts
+++ packages/interactor/src/page.ts
@@ -10,7 +10,7 @@ const PageInteractor = createInteractor('page')({
   }
 });
 
-export const Page = Object.assign(PageInteractor(), {
+const PageInteractorInstance = Object.assign(PageInteractor(), {
   visit(path = '/'): Interaction<void> {
     return interaction(`visiting ${JSON.stringify(path)}`, async () => {
       let appUrl = bigtestGlobals.appUrl;
@@ -42,3 +42,33 @@ export const Page = Object.assign(PageInteractor(), {
     });
   }
 });
+
+/**
+ * This {@link Interactor} can be used to assert on global properties of the
+ * page. When using the BigTest test runner, it can also be used for
+ * interacting with the page itself, for example through nagivation.
+ *
+ * ### Example
+ *
+ * ``` typescript
+ * await Page.has({ title: 'Welcome to my app!' });
+ * ```
+ *
+ * Navigation, for BigTest test runner only:
+ *
+ * ``` typescript
+ * await Page.visit('/archive');
+ * ```
+ *
+ * ### Filters
+ *
+ * - `title`: *string* – the title of the document
+ * - `url`: *string* – the URL of the document
+ *
+ * ### Actions
+ *
+ * - `visit(path: string)`: *{@link Interaction}* – visit the given path in the test frame, BigTest runner only.
+ *
+ * @category Interactor
+ */
+export const Page = PageInteractorInstance;
