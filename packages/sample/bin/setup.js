1d3dc4cf471e49fa4d98c4e89c3a14dcf5543859
minkimcello <minkimcello@gmail.com>
2020-12-03 18:02:53 -0500

    Enhance CLI output

--- packages/sample/bin/setup.js
+++ packages/sample/bin/setup.js
@@ -1,36 +1,19 @@
 #!/usr/bin/env node
 
-// const chalk = require('chalk');
-const spawn = require('cross-spawn');
+const chalk = require('chalk');
 const fs = require('fs');
 const rmrf = require('rimraf');
+const spawn = require('cross-spawn');
 
-const TARGET_DIR = 'bigtest-sample';
-const SOURCE_DIR = `${TARGET_DIR}/node_modules/${TARGET_DIR}`;
+const { message, yarn, TARGET_DIR, SOURCE_DIR } = require('./constants');
+const Spinner = require('./helper');
 
-let yarn = false;
+const animate = (msgs) => new Spinner(msgs);
 
-function setYarn() {
-  console.log('setyarn');
-  return new Promise((resolve, _) => {
-    if (
-      process.argv[2] === '--yarn' ||
-      process.argv[2] === '-Y'
-    ) {
-      yarn = true;
-      console.log('yarn set to true');
-    } else {
-      console.log('yarn kept as false');
-    }
-    resolve();
-  })
-};
-
-function populate() {
-  console.log('populate');
+function populate(message) {
   return new Promise(async (resolve, reject) => {
     if (fs.existsSync(TARGET_DIR)) {
-      reject('bigtest-sample directory already exists');
+      reject('directory \'bigtest-sample\' already exists');
     } else {
       fs.mkdirSync(TARGET_DIR);
       const installer = {
@@ -38,69 +21,87 @@ function populate() {
         version: '0.0.0',
         private: true,
         dependencies: {
-          '@minkimcello/georgia': '*'
+          'bigtest-sample': '*'
         }
       };
       fs.writeFileSync(
         `${TARGET_DIR}/package.json`,
         JSON.stringify(installer, null, 2)
       );
+      console.log(message);
       resolve();
     };
   });
 };
 
-function migrate() {
-  console.log('migrate');
+function migrate(messages) {
   return new Promise((resolve, _) => {
+    let loading = animate(messages);
+    loading.start();
     const lockfile = yarn ? 'yarn.lock' : 'package-lock.json';
     rmrf.sync(`${TARGET_DIR}/${lockfile}`);
     rmrf.sync(`${TARGET_DIR}/package.json`);
 
-    const hard_list = ['cypress/', 'public/', 'src/', 'bigtest.json', 'cypress.json', 'package.json'];
+    // todo: refactor hard coded list
+    const hard_list = ['cypress/', 'public/', 'src/', 'bigtest.json', 'cypress.json', 'package.json']; 
     hard_list.forEach(file => {
       if(file==='package.json'){
         fs.renameSync(`${SOURCE_DIR}/bin/${file}`, `${TARGET_DIR}/${file}`)
       } else {
         fs.renameSync(`${SOURCE_DIR}/${file}`, `${TARGET_DIR}/${file}`);
-      }
+      };
     });
 
     rmrf.sync(`${TARGET_DIR}/node_modules/`);
+    loading.stop();
     resolve();
   })
-}
+};
 
-function install() {
+function install(messages) {
   return new Promise((resolve, reject) => {
     let command = yarn ? 'yarn' : 'npm';
+    let loading = animate(messages);
+    loading.start();
     const install = spawn(command, ['install'], {
       cwd: TARGET_DIR,
-      stdio: 'inherit'
+      stdio: 'ignore'
     });
     install.on('close', code => {
+      loading.stop();
       if (code !== 0) {
-        reject('did not install successfully');
-        return;
+        reject('Error while installing');
       }
       resolve();
     });
   });
 };
 
-function cleanup(e) {
-  console.log('Aborting because', e);
-  rmrf.sync(TARGET_DIR);
-  console.log('Deleted generated files.');
+function abort(messages, err) {
+  if(err){
+    console.log(chalk`\n{red Error}: {yellow ${err}}\n`);
+  }
+  if (messages) {
+    let loading = animate(messages);
+    loading.start();
+    rmrf.sync(TARGET_DIR);
+    loading.stop();
+  } else {
+    console.log(message.abort);
+  }
   process.exit(1);
-};
+}
 
 async function run() { 
-  await setYarn().catch(e => cleanup(e));
-  await populate().catch(e => cleanup(e));
-  await install().catch(e => cleanup(e));
-  await migrate().catch(e => cleanup(e));
-  await install().catch(e => cleanup(e));
+  await populate(message.creating_dir)
+    .catch(err => abort(undefined, err));
+  await install(message.downloading_repo)
+    .catch(err => abort(message.abort, err));
+  await migrate(message.organizing_files)
+    .catch(() => abort(message.abort));
+  await install(message.installing_dep)
+    .catch(err => abort(message.abort, err));
+  console.log(message.success);
 };
 
 run();
