ada894f41238b6d4bfaf4b6ca5ec1a5b59c120fb
Jonas Nicklas <jonas@jnicklas.com>
2020-12-03 17:11:29 +0100

    Fix race condition in chromedriver

--- packages/webdriver/src/until-url-available.ts
+++ packages/webdriver/src/until-url-available.ts
@@ -1,6 +1,10 @@
 import { Operation, spawn, timeout } from 'effection';
 import { fetch } from '@effection/fetch';
 
+class TimeoutError extends Error {
+  name = 'TimeoutError';
+}
+
 /**
  * An operation that completes when the server at `url` begins
  * returning successful responses to an HTTP GET request.
@@ -8,17 +12,23 @@ import { fetch } from '@effection/fetch';
 export function* untilURLAvailable(url: string, maxWait: number): Operation<void> {
   yield spawn(function* () {
     yield timeout(maxWait);
-    throw new Error(`timed out waiting ${maxWait}ms for ${url} to become available`)
+    throw new TimeoutError(`timed out waiting ${maxWait}ms for ${url} to become available`)
   });
   while (true) {
     try {
-      let response: Response = yield fetch(url);
+      let response: Response = yield function*() {
+        yield spawn(function* () {
+          yield timeout(maxWait);
+          throw new TimeoutError(`timed out waiting ${maxWait}ms for ${url} to become available`)
+        });
+        return yield fetch(url);
+      };
       if (response.ok) {
         return;
       }
     } catch (error) {
       /* FetchError is ok. Sever may not yet be accepting connections */
-      if (error.name !== 'FetchError') {
+      if (error.name !== 'TimeoutError' && error.name !== 'FetchError') {
         throw error;
       }
     }
