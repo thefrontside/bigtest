0239379e8b15e4598912595731684437d3308af2
Jacob Bolda <me@jacobbolda.com>
2020-12-07 10:14:27 -0600

    using extends fixes types

--- packages/interactor/src/focused.ts
+++ packages/interactor/src/focused.ts
@@ -1,5 +1,4 @@
 import { perform } from './perform';
-import { ActionFn } from './specification';
 
 /**
  * Helper function for focused filters, returns whether the given element is focused.
@@ -8,12 +7,10 @@ export function focused(element: Element): boolean {
   return element.ownerDocument.activeElement === element;
 }
 
-export const focus: ActionFn<any> =
-  perform(<T extends keyof HTMLElementTagNameMap>(element: HTMLElementTagNameMap[T]) => {
+export const focus = perform(<E extends HTMLElement>(element: E) => {
     element.focus();
 });
 
-export const blur: ActionFn<any> =
-  perform(<T extends keyof HTMLElementTagNameMap>(element: HTMLElementTagNameMap[T]) => {
+export const blur = perform(<E extends HTMLElement>(element: E) => {
     element.blur();
 });
