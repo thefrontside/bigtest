ca653c654ba1ee8e30c2dc8721a39106b46ff418
Jacob Bolda <me@jacobbolda.com>
2020-12-10 11:17:30 -0600

    add docs to focus/blur functions

--- packages/interactor/src/focused.ts
+++ packages/interactor/src/focused.ts
@@ -7,10 +7,32 @@ export function focused(element: Element): boolean {
   return element.ownerDocument.activeElement === element;
 }
 
+/**
+ * Helper function for focus action, performs `element.focus()` on the selected element.
+ * 
+ * Can be used with object property value shorthand in your interactor actions.
+ * 
+ * ```js
+ * actions: {
+ *   focus,
+ * }
+ * ```
+ */
 export const focus = perform(<E extends HTMLElement>(element: E) => {
     element.focus();
 });
 
+/**
+ * Helper function for blur action, performs `element.blur()` on the selected element.
+ * 
+ * Can be used with object property value shorthand in your interactor actions.
+ * 
+ * ```js
+ * actions: {
+ *   focus,
+ * }
+ * ```
+ */
 export const blur = perform(<E extends HTMLElement>(element: E) => {
     element.blur();
 });
