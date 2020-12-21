83c17969961aaedfd2cf82db45cea8a4159196bd
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2020-12-21 21:39:23 +0100

    Removes typedoc generated API from docusaurus

--- website/sidebars.js
+++ website/sidebars.js
@@ -4,12 +4,7 @@ module.exports = {
     'interactors/built-in-dom',
     'interactors/locators-filters-actions',
     'interactors/write-your-own',
-    'interactors/integrations',
-    {
-      "type": "category",
-      "label": "API",
-      "items": require('./sidebars/typedoc-interactor-sidebar')
-    },
+    'interactors/integrations'
   ],
   platform: [
     'platform/installation',
