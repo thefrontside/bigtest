a932530ed4c54809c26ce95c1b6ce1932cf92042
Paul Cowan <paul.cowan@disclosurescotland.gov.scot>
2021-01-11 13:25:45 +0000

    format CreateAgentValue

--- packages/agent/app/agent.ts
+++ packages/agent/app/agent.ts
@@ -5,7 +5,12 @@ import { run } from './runner';
 import { Controller, Operation, OperationFn, Sequence } from 'effection';
 
 // union of values that is yielded from createAgent
-type CreateAgentValue = Operation<Agent> | OperationFn<void> | Sequence<void> | PromiseLike<void> | Controller<void>;
+type CreateAgentValue =
+  | Operation<Agent>
+  | OperationFn<void>
+  | Sequence<void> 
+  | PromiseLike<void> 
+  | Controller<void>;
 
 export function* createAgent(queryParams: QueryParams): Generator<CreateAgentValue, void, Agent> {
   console.log('[agent] connecting to', queryParams.connectTo);
