ab6d1a10ad275930b89df92df0be3936b0b6b4ca
minkimcello <minkimcello@gmail.com>
2021-01-11 13:04:25 -0500

    Refactor start and setup scripts with effection

--- sample/bin/setup.js
+++ sample/bin/setup.js
@@ -1,119 +1,117 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk');
 const fs = require('fs');
 const rmrf = require('rimraf');
 const spawn = require('cross-spawn');
+const { main, MainError } = require('@effection/node');
+const { once } = require('@effection/events');
 
-const { message, yarn, TARGET_DIR, SOURCE_DIR, startScript } = require('./constants');
-const Spinner = require('./helper');
+const { 
+  messages, yarn, TARGET_DIR, SOURCE_DIR, startScript
+} = require('./constants');
+const { formatErr, logSuccess, Spinner } = require('./helper');
 const { version } = require('../package.json');
 
 const animate = (msgs) => new Spinner(msgs);
 
-function populate(message) {
-  return new Promise((resolve, reject) => {
-    if (fs.existsSync(TARGET_DIR)) {
-      reject('directory \'bigtest-sample\' already exists');
-    } else {
-      fs.mkdirSync(TARGET_DIR);
-      const installer = {
-        name: 'bigtest-sample',
-        version: '0.0.0', 
-        dependencies: {
-          'bigtest-sample': version
-        }
+async function populate(message) {
+  if (fs.existsSync(TARGET_DIR)) {
+    throw new MainError({message: `${formatErr('directory \'bigtest-sample\' already exists')}\n${messages.abort}`});
+  } else {
+    fs.mkdirSync(TARGET_DIR);
+    const installer = {
+      name: 'bigtest-sample-temporary-package-name',
+      version: '0.0.0',
+      dependencies: {
+        'bigtest-sample': version
+      }
+    };
+    fs.writeFileSync(
+      `${TARGET_DIR}/package.json`,
+      JSON.stringify(installer, null, 2)
+    );
+    console.log(message);
+  };
+};
+
+async function migrate(messages) {
+  let loading = animate(messages);
+  loading.start();
+  const lockfile = yarn ? 'yarn.lock' : 'package-lock.json';
+  rmrf.sync(`${TARGET_DIR}/${lockfile}`);
+  rmrf.sync(`${TARGET_DIR}/package.json`);
+
+  fs.readdirSync(SOURCE_DIR).filter(file => {
+    return file !== 'bin';
+  }).forEach(file => {
+    if(file==='package.json'){
+      const {
+        name, version, description, repository, author, license, 
+        main, scripts, devDependencies, eslintConfig, browserslist,
+        babel, jest
+      } = require(`${SOURCE_DIR}/${file}`);
+
+      scripts.start = startScript;
+
+      const pkgjson = {
+        name, version, private: true, description, repository, author, 
+        license, main, scripts, devDependencies, eslintConfig, browserslist,
+        babel, jest
       };
-      fs.writeFileSync(
-        `${TARGET_DIR}/package.json`,
-        JSON.stringify(installer, null, 2)
-      );
-      console.log(message);
-      resolve();
+
+      fs.writeFileSync(`${TARGET_DIR}/package.json`, JSON.stringify(pkgjson, null, 2));
+    } else {
+      fs.renameSync(`${SOURCE_DIR}/${file}`, `${TARGET_DIR}/${file}`);
     };
   });
+
+  rmrf.sync(`${TARGET_DIR}/node_modules/`);
+  loading.stop();
+  logSuccess(messages[1]);
 };
 
-function migrate(messages) {
-  return new Promise((resolve, _) => {
-    let loading = animate(messages);
-    loading.start();
-    const lockfile = yarn ? 'yarn.lock' : 'package-lock.json';
-    rmrf.sync(`${TARGET_DIR}/${lockfile}`);
-    rmrf.sync(`${TARGET_DIR}/package.json`);
-
-    fs.readdirSync(SOURCE_DIR).filter(file => {
-      return file !== 'bin';
-    }).forEach(file => {
-      if(file==='package.json'){
-        const {
-          name, version, description, repository, author, license, 
-          main, scripts, devDependencies, eslintConfig, browserslist,
-          babel, jest
-        } = require(`${SOURCE_DIR}/${file}`);
-
-        scripts.start = startScript;
-
-        const pkgjson = {
-          name, version, private: true, description, repository, author, 
-          license, main, scripts, devDependencies, eslintConfig, browserslist,
-          babel, jest
-        };
-
-        fs.writeFileSync(`${TARGET_DIR}/package.json`, JSON.stringify(pkgjson, null, 2));
-      } else {
-        fs.renameSync(`${SOURCE_DIR}/${file}`, `${TARGET_DIR}/${file}`);
-      };
-    });
-
-    rmrf.sync(`${TARGET_DIR}/node_modules/`);
-    loading.stop();
-    resolve();
-  });
-};
-
-function install(messages) {
-  return new Promise((resolve, reject) => {
+function* install(messages) {
+  let loading = animate(messages);
+  try {
     let command = yarn ? 'yarn' : 'npm';
-    let loading = animate(messages);
     loading.start();
     const install = spawn(command, ['install'], {
       cwd: TARGET_DIR,
       stdio: 'ignore'
     });
-    install.on('close', code => {
-      loading.stop();
-      if (code !== 0) {
-        reject('Error while installing');
-      }
-      resolve();
-    });
-  });
+    let [code] = yield once(install, 'close');
+    if (code !== 0) {
+      throw new MainError({ message: `${formatErr('Error while installing')}` });
+    };
+  } finally {
+    loading.stop();
+  };
+  logSuccess(messages[1]);
 };
 
-function abort(err, clean, messages) {
-  console.log(chalk`\n{red Error}: {yellow ${err}}\n`);
-  if(!clean) {
-    console.log(messages);
-  }
-  if (clean) {
-    let loading = animate(messages);
+function clean(e){
+  let message = messages.deleting;
+  let loading = animate(message[0]);
+  try {
     loading.start();
     rmrf.sync(TARGET_DIR);
+  } finally {
     loading.stop();
-  }
-  process.exit(1);
-}
-
-async function run() { 
-  await populate(message.creating_dir)
-    .catch(err => abort(err, false, message.abort));
-  await install(message.downloading_repo)
-    .catch(err => abort(err, true, message.deleting));
-  await migrate(message.organizing_files);
-  await install(message.installing_dep)
-    .catch(err => abort(err, true, message.deleting));
-  console.log(message.success);
+  };
+  logSuccess(message[1]);
+  throw new MainError(e);
 };
 
-run();
+function* run() {
+  yield populate(messages.creating_dir);
+  try {
+    yield install(messages.downloading_repo);
+    yield migrate(messages.organizing_files);
+    yield install(messages.installing_dep);
+  } catch(e) {
+    clean(e);
+  }
+  console.log(messages.success);
+};
+
+main(run);
--- sample/bin/start.js
+++ sample/bin/start.js
@@ -1,26 +1,26 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk');
-const { yarn } = require('./constants');
+const { main, MainError } = require('@effection/node');
+const { once } = require('@effection/events');
+
+const { yarn, formatErr } = require('./constants');
 const spawn = require('cross-spawn');
 
-function install() {
-  return new Promise((resolve, reject) => {
-    let command = yarn;
-    const install = spawn(command, ['install'], {
-      stdio: 'inherit'
-    });
-    install.on('close', code => {
-      if (code !== 0) {
-        reject('Error while installing');
-      }
-      resolve();
-    });
+function* install() {  
+  let command = yarn ? 'yarn' : 'npm';
+  const install = spawn(command, ['install'], {
+    stdio: 'inherit'
   });
+  let [code] = yield once(install, 'close');  
+  if (code !== 0) {
+    throw new MainError({ 
+      message: `${formatErr('Error while installing')}`
+    });
+  };
 };
 
-function start() {
-  return new Promise(async (resolve) => {
+async function start() {
+  try {
     let port;
     if(process.argv.includes('-p')){
       port = process.argv.indexOf('-p');
@@ -29,21 +29,17 @@ function start() {
     if(port){
       args.push(...process.argv.slice(port, port + 2))
     };
-    await spawn.sync('node', args, {
+    spawn.sync('node', args, {
       stdio: 'inherit'
     });
-    resolve();
-  })
+  } catch(e) {
+    throw new MainError({ message: `${formatErr(e)}`});
+  }
 };
 
-function abort(e) {
-  console.log(chalk`\n{red Error}: {yellow ${e}}\n`);
-  process.exit(1);
+function* run() {
+  yield install();
+  yield start();
 };
 
-async function run() {
-  await install().catch(e => abort(e));
-  await start();
-};
-
-run();
+main(run);
