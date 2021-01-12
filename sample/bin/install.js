05b710eec3c90b0e6f56e438e75f9d24ab733f1e
minkimcello <minkimcello@gmail.com>
2021-01-12 18:48:59 -0500

    Remove cross-spawn in favor of effection exec in install script

--- sample/bin/install.js
+++ sample/bin/install.js
@@ -1,20 +1,18 @@
-const { MainError } = require('@effection/node');
-const { once } = require('@effection/events');
-const spawn = require('cross-spawn');
-const { yarn, formatErr } = require('./constants');
+const { exec } = require('@effection/node');
+const { subscribe } = require('@effection/subscription');
+const { spawn } = require('effection');
+const { yarn } = require('./constants');
 
-function* install(cwd) {  
+function* install({ cwd, stdio = 'ignore' }) {  
   let command = yarn ? 'yarn' : 'npm';
-  const install = spawn(command, ['install'], {
-    stdio: 'ignore',
-    cwd
-  });
-  let [code] = yield once(install, 'close');  
-  if (code !== 0) {
-    throw new MainError({ 
-      message: `${formatErr('Error while installing')}`
-    });
+  let install = yield exec(`${command} install`, { cwd });
+  if(stdio === 'inherit'){
+    yield spawn(subscribe(install.stdout).forEach((data) => {
+      process.stdout.write(data);
+      return Promise.resolve();
+    }));
   };
+  yield install.expect();
 };
 
 module.exports = {
