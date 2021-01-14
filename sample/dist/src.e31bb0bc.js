b27f03ab17aba4b7717df81fe091dc49393c38f8
minkimcello <minkimcello@gmail.com>
2021-01-14 10:12:27 -0500

    Include stderr in install script

--- sample/dist/src.e31bb0bc.js
+++ sample/dist/src.e31bb0bc.js
@@ -29931,7 +29931,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55811" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54160" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
