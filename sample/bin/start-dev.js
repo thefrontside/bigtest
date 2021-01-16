67fd22c1f5898e83aa1684459f128455757a0ea8
minkimcello <minkimcello@gmail.com>
2021-01-15 21:28:11 -0500

    Dev-start copies app-pkg.json as package.json and then install/start

--- sample/bin/start-dev.js
+++ sample/bin/start-dev.js
@@ -3,20 +3,26 @@
 const { daemon, main } = require('@effection/node');
 const { spawn } = require('effection');
 const { subscribe } = require('@effection/subscription');
+const fs = require('fs');
+const fsp = fs.promises;
+const rmrf = require('rmfr');
 
 const { install } = require('./install');
 const { yarn } = require('./constants');
 
-function* start() {
+function* dev() {
   let command = yarn ? 'yarn' : 'npx';
 
   let port;
-  let args = ['parcel', './src/index.html'];
+  let args = ['--cwd app start'];
 
   if(process.argv.includes('-p')){
     port = process.argv.indexOf('-p');
     args.push(...process.argv.slice(port, port + 2))
   };
+
+  yield fsp.copyFile('app/app-pkg.json', 'app/package.json');
+  yield install({ cwd: 'app/', stdio: 'inherit' });
 
   let { stdout, stderr } = yield daemon(`${command} ${args.join(' ')}`);
 
@@ -33,9 +39,4 @@ function* start() {
   yield;
 };
 
-function* run() {
-  yield install({ stdio: 'inherit' });
-  yield start();
-};
-
-main(run);
+main(dev);
