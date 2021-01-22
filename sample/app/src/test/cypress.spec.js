63fa3a752286b5b138ccf9d7b896e1a561367111
minkimcello <minkimcello@gmail.com>
2021-01-22 13:04:49 -0500

    Add @bigtest/cypress and update cypress test

--- sample/app/src/test/cypress.spec.js
+++ sample/app/src/test/cypress.spec.js
@@ -1,8 +1,9 @@
-import { Button } from 'bigtest';
+import { Button } from '@bigtest/cypress';
 
 describe('Interactors with Cypress', () => {
   it('Interactors with Cypress', () => {
-    cy.visit('/')
-    cy.expect(Button('SIGN IN').exists())
-  })
+    cy.visit('/');
+    cy.do(Button('SIGN IN').click());
+    cy.expect(Button('SIGN IN').absent());
+  });
 });
