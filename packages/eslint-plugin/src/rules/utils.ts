f7f554d9d7ce813e457211b4455b930ff9b098c4
Paul Cowan <paul.cowan@cutting.scot>
2021-02-01 11:12:52 +0000

    fix linking of @bigtest/eslint-plugin rule to docs

--- packages/eslint-plugin/src/rules/utils.ts
+++ packages/eslint-plugin/src/rules/utils.ts
@@ -2,14 +2,23 @@ import {
   ESLintUtils,
 } from '@typescript-eslint/experimental-utils';
 import path from 'path';
+import { execSync } from 'child_process';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { version } = require('../../package.json');
+const REPO_URL = 'https://github.com/thefrontside/bigtest';
 
-const REPO_URL = 'https://github.com/thefrontside/bigtest/eslint-plugin';
+export const getCurrentBranch = (): string => {
+  try {
+    return execSync('git branch --show-current', { timeout: 1000 }).toString().trim();
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
 
 export const createRule = ESLintUtils.RuleCreator(name => {
   let ruleName = path.parse(name).name;
 
-  return `${REPO_URL}/blob/v${version}/docs/rules/${ruleName}.md`;
+  let currentBranch = getCurrentBranch();
+
+  return `${REPO_URL}/blob/${currentBranch}/packages/eslint-plugin/docs/rules/${ruleName}.md`;
 });
