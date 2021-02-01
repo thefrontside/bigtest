e4cdb828e7a3f3507a821980347c039f90b1c5a9
Paul Cowan <paul.cowan@cutting.scot>
2021-02-01 11:31:38 +0000

    scope @bigtest rules

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,14 +1,14 @@
 import { requireDefaultTextExport } from './rules/require-default-export';
 
 export const rules = {
-  'require-default-export': requireDefaultTextExport
+  '@bigtest/require-default-export': requireDefaultTextExport
 };
 
 export const configs = {
   root: true,
   recommended: {
     rules: {
-      'bigtest/require-default-export': 2
+      '@bigtest/require-default-export': 2
     }
   }
 }
