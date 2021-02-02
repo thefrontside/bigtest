82a221d1c9243a21a3b245d6b479241089f0207f
Paul Cowan <paul.cowan@cutting.scot>
2021-02-02 09:37:18 +0000

    throw MainError

--- packages/cli/src/ensure-configuration.ts
+++ packages/cli/src/ensure-configuration.ts
@@ -1,15 +1,11 @@
 import { ProjectOptions } from '@bigtest/project';
+import { MainError } from '@effection/node';
 import fs from 'fs';
 
 export const ensureConfiguration = (config: ProjectOptions): void => {
   if (typeof config.tsconfig !== 'undefined' && fs.existsSync(config.tsconfig) === false) {
-    let errorMessage = `The \`tsconfig\` option of bigtest.json (\`${config.tsconfig}\`) is invalid.`;
+    let message = `The \`tsconfig\` option of bigtest.json (\`${config.tsconfig}\`) is invalid.`;
     
-    console.error(errorMessage);
-
-    // is process.exit(1); sufficient from an effection standpoint or
-    // or should I use
-    // throw new Error(errorMessage);
-    process.exit(1);
+    throw new MainError({ exitCode: 1, message });
   }
 }
