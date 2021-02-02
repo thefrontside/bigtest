d4b53ed8ecbbba54f6013e12400ab0a4e040684b
Paul Cowan <paul.cowan@cutting.scot>
2021-02-02 17:51:41 +0000

    remove hardcoded tsconfig value in error message

--- packages/cli/src/ensure-configuration.ts
+++ packages/cli/src/ensure-configuration.ts
@@ -4,7 +4,7 @@ import fs from 'fs';
 
 export const ensureConfiguration = (config: ProjectOptions): void => {
   if (typeof config.tsconfig !== 'undefined' && fs.existsSync(config.tsconfig) === false) {
-    let message = `The \`tsconfig\` option of bigtest.json (\`${config.tsconfig}\`) is invalid.`;
+    let message = `The \`tsconfig\` option of (\`${config.tsconfig}\`) is invalid.`;
     
     throw new MainError({ exitCode: 1, message });
   }
