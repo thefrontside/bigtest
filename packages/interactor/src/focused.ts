1e22a72eb9a2de879d0a9d055ff37bf520448470
Charles Lowell <cowboyd@frontside.io>
2021-02-10 11:37:28 +0200

    Deprecate focused, focus, and blur helpers

--- packages/interactor/src/focused.ts
+++ packages/interactor/src/focused.ts
@@ -6,6 +6,7 @@ import { Interactor } from './specification';
  * Helper function for focused filters, returns whether the given element is focused.
  */
 export function focused(element: Element): boolean {
+  console.warn("usage of the focused() helper is deprecated, and will be removed from the public API. Instead, switch your interactor to extend the `HTML` interactor to inherit the `focused` filter");
   return element.ownerDocument.activeElement === element;
 }
 
@@ -21,6 +22,7 @@ export function focused(element: Element): boolean {
  * ```
  */
 export async function focus<E extends HTMLElement>(interactor: Interactor<E, any>): Promise<void> {
+  console.warn("usage of the focus() helper is deprecated, and will be removed from the public API. Instead, switch your interactor to extend the `HTML` interactor to inherit the `focus()` action");
   await interactor.perform((element) => element.focus())
 };
 
@@ -36,5 +38,6 @@ export async function focus<E extends HTMLElement>(interactor: Interactor<E, any
  * ```
  */
 export async function blur<E extends HTMLElement>(interactor: Interactor<E, any>): Promise<void> {
+  console.warn("usage of the blur() helper is deprecated, and will be removed from the public API. Instead, switch your interactor to extend the `HTML` interactor to inherit the `blur()` action");
   await interactor.perform((element) => element.blur())
 };
