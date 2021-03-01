34bde3e16020bf82649355061be0f44e41a1967b
minkimcello <minkimcello@gmail.com>
2021-03-01 18:54:46 -0500

    Update bigtest, cypress, and jest tests

--- sample/app/src/test/jest.test.js
+++ sample/app/src/test/jest.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App';
-import { Button } from 'bigtest';
+import { Button, Heading, Link } from 'bigtest';
 
 describe('Interactor with Jest', () => {
-  it('displays submitted data from modal', async () => {
-    render(<App />)
+  beforeEach(() => {
+    render(<App />);
+  });
+  it('click sign in button', async () => {
     await Button('SIGN IN').click();
+    await Button('SIGN IN').absent();
+    await Button('LOG OUT').exists();
+  });
+  it('navigate to about page', async () => {
+    await Link('/about').click();
+    await Heading('About page').exists();
   });
 });
