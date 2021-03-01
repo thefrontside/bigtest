eb0f049faf155ae7fef5f711c2c2b378b191663f
minkimcello <minkimcello@gmail.com>
2021-03-01 18:54:16 -0500

    Implement router to app

--- sample/app/src/App.jsx
+++ sample/app/src/App.jsx
@@ -1,22 +1,53 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
 const App = () => {
   let [isSignedIn, setIsSignedIn] = React.useState(false)
   return (
-    <div className="App">
-      {!isSignedIn &&
-        <button onClick={() => setIsSignedIn(true)}>
-          SIGN IN
-        </button>
-      }
-
-      {isSignedIn &&
-        <button onClick={() => setIsSignedIn(false)}>
-          LOG OUT
-        </button>
-      }
-    </div>
+    <Router>
+      <div className="App">
+        <div className="header">
+          <nav>
+            <div className="label">
+              Nav
+            </div>
+            <Link to="/">/home</Link>
+            <Link to="/about">/about</Link>
+          </nav>
+          <div className="button">
+            <div className="label">
+              Button
+            </div>
+            {!isSignedIn &&
+              <button onClick={() => setIsSignedIn(true)}>
+                SIGN IN
+              </button>
+            }
+            {isSignedIn &&
+              <button onClick={() => setIsSignedIn(false)}>
+                LOG OUT
+              </button>
+            }
+          </div>
+        </div>
+        <div className="body">
+          <Switch>
+            <Route path="/about">
+              <h1>About page</h1>
+            </Route>
+            <Route path="/">
+              <h1>Home page</h1>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 };
 
