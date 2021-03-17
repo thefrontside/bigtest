8735eae2075fcce4636662981dd2dce7e30b59eb
wKich <w@kich.dev>
2021-03-17 11:21:14 +0100

    fix: postcss options

--- packages/inspector/webpack.config.js
+++ packages/inspector/webpack.config.js
@@ -17,7 +17,14 @@ module.exports = {
       },
       {
         test: /\.css/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: { postcssOptions: { plugins: [require("tailwindcss"), require("autoprefixer")] } }
+          }
+        ],
       },
     ],
   },
