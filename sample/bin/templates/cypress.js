7fccd4ddff3adc173f94e1a6711cc5070c6cc5e3
minkimcello <minkimcello@gmail.com>
2021-03-18 12:00:26 -0400

    Add babel core to cypress template

--- sample/bin/templates/cypress.js
+++ sample/bin/templates/cypress.js
@@ -6,6 +6,7 @@ const cypressTemplate = ({ dependencies }) => {
         "test:cypress": "start-server-and-test 'npm run start -- -p 3000' http://localhost:3000 cypress:run",
       },
       "dependencies": {
+        "@babel/core": `${dependencies['@babel/core']}`,
         "@bigtest/cypress": `${dependencies['@bigtest/cypress']}`,
         "eslint-plugin-cypress": `${dependencies['eslint-plugin-cypress']}`,
         "start-server-and-test": `${dependencies['start-server-and-test']}`
