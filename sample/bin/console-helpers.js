8aa6991f28c658002d0864e05953ce7e4228d4ec
minkimcello <minkimcello@gmail.com>
2021-04-06 12:00:34 -0400

    Refactor sample app scripts to effection v2

--- sample/bin/console-helpers.js
+++ sample/bin/console-helpers.js
@@ -1,21 +1,23 @@
 const chalk = require('chalk');
-const { spawn, timeout } = require('effection');
+const { sleep } = require('effection');
 
-function* spin(message, operation){
-  yield spawn(function* () {
-    const spinner = ['⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷'];
-    let frameNumber = 0;
-    try {
-      while(true) {
-        let spinnerGlyph = chalk`{yellow ${spinner[frameNumber++ % spinner.length]}}`;
-        process.stdout.write('\u001b[0G' + spinnerGlyph + ' ' + message + '\u001b[0m');
-        yield timeout(30);
+function spin(message, operation){
+  return function*(task){
+    yield task.spawn(function* () {
+      const spinner = ['⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷'];
+      let frameNumber = 0;
+      try {
+        while(true) {
+          let spinnerGlyph = chalk`{yellow ${spinner[frameNumber++ % spinner.length]}}`;
+          process.stdout.write('\u001b[0G' + spinnerGlyph + ' ' + message + '\u001b[0m');
+          yield sleep(30);
+        };
+      } finally {
+        process.stdout.write('\u001b[0G\u001b[2K');
       };
-    } finally {
-      process.stdout.write('\u001b[0G\u001b[2K');
-    };
-  });
-  return yield operation;
+    });
+    return yield operation;
+  };
 };
 
 const formatErr = (err) => {
--- sample/bin/install.js
+++ sample/bin/install.js
@@ -1,21 +1,19 @@
 const { exec } = require('@effection/node');
-const { subscribe } = require('@effection/subscription');
-const { spawn } = require('effection');
 
-function* install({ cwd, stdio }) {  
-  let command = process.argv.includes('-Y') || process.argv.includes('-yarn') ? 'yarn' : 'npm';
-  let install = yield exec(`${command} install`, { cwd });
-  if(stdio === 'inherit'){
-    yield spawn(subscribe(install.stdout).forEach((data) => {
-      process.stdout.write(data);
-      return Promise.resolve();
-    }));
-    yield spawn(subscribe(install.stderr).forEach((data) => {
-      process.stderr.write(data);
-      return Promise.resolve();
-    }));
-  };
-  yield install.expect();
+function install({ cwd, stdio }) {  
+  return function*(task){
+    let command = process.argv.includes('-Y') || process.argv.includes('-yarn') ? 'yarn' : 'npm';
+    let install = exec(`${command} install`, { cwd }).run(task);
+    if(stdio === 'inherit'){
+      yield task.spawn(install.stdout.forEach((data) => {
+        process.stdout.write(data);
+      }));
+      yield task.spawn(install.stderr.forEach((data) => {
+        process.stderr.write(data);
+      }));
+    };
+    yield install.expect();
+  }
 };
 
 module.exports = {
