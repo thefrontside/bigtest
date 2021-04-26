c6c364ef33363b4ed05622fcb446aeabc8762edb
minkimcello <minkimcello@gmail.com>
2021-04-26 15:37:47 -0400

    Add test in matching matcher for deprecated format function

--- packages/interactor/test/matchers/matching.test.ts
+++ packages/interactor/test/matchers/matching.test.ts
@@ -3,21 +3,38 @@ import expect from 'expect';
 import { dom } from '../helpers';
 
 import { HTML, matching } from '../../src/index';
+import { Matcher } from '../../src/matcher';
+
+function matchingWithDeprecatedFormat(regexp: RegExp): Matcher<string> {
+  return {
+    match(actual: string): boolean {
+      return actual.match(regexp) != null;
+    },
+    format(): string {
+      return `matching ${regexp}`;
+    },
+  }
+}
 
 describe('@bigtest/interactor', () => {
+  beforeEach(() => {
+    dom(`
+      <div title="hello world"></div>
+      <div title="what the heck"></div>
+    `);
+  });
+  
   describe('matching', () => {
-    it('can provide description', () => {
-      expect(matching(/hello/).description()).toEqual('matching /hello/');
-    });
-
     it('can check whether the given string matching', async () => {
-      dom(`
-        <div title="hello world"></div>
-        <div title="what the heck"></div>
-      `);
-
       await HTML({ title: matching(/he(llo|ck)/) }).exists();
       await expect(HTML({ title: matching(/blah/) }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError')
     });
   });
+
+  describe('matching with deprecated format() API', () => {
+    it('can check whether the given string matching', async () => {
+      await HTML({ title: matchingWithDeprecatedFormat(/he(llo|ck)/) }).exists();
+      await expect(HTML({ title: matchingWithDeprecatedFormat(/blah/) }).exists()).rejects.toHaveProperty('name', 'NoSuchElementError')
+    });
+  })
 });
