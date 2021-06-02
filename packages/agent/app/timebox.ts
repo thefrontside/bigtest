fad00502717afad0b7c59a1770114d34a6c210e9
minkimcello <minkimcello@gmail.com>
2021-06-02 19:12:02 -0400

    Remove redundant error throws in timebox

--- packages/agent/app/timebox.ts
+++ packages/agent/app/timebox.ts
@@ -3,9 +3,6 @@ import { Operation, spawn, timeout } from 'effection';
 export function* timebox<T>(operation: Operation<T>, timelimit: number): Operation<T> {
   yield spawn(function*(): Operation<void> {
     yield timeout(timelimit);
-    let error = new Error(`timelimit of ${timelimit}ms exceeded`);
-    error.name = 'TimeoutError';
-    throw error;
   });
 
   return yield operation;
