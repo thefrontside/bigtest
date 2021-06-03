ca5f19200c0fd1df27bef73aa35fc56e81043513
minkimcello <minkimcello@gmail.com>
2021-06-03 15:03:12 -0400

    Remove yield function syntax from timebox

--- packages/agent/app/timebox.ts
+++ packages/agent/app/timebox.ts
@@ -1,9 +1,9 @@
-import { Operation, spawn, timeout } from 'effection';
+import { Operation, spawn, timeout } from '../node_modules/effection';
 
 export function* timebox<T>(operation: Operation<T>, timelimit: number): Operation<T> {
-  yield spawn(function*(): Operation<void> {
-    yield timeout(timelimit);
-  });
+  yield spawn(
+    yield timeout(timelimit)
+  );
 
   return yield operation;
 }
