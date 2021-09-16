243ca0c8bf63935ae18ed8b5044c25d89cfcfbd3
Jeffrey Cherewaty <jeffrey@frontside.com>
2021-09-16 10:20:59 -0500

    Replace performance package

--- packages/performance/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# @bigtest/performance
-
-## 0.6.0
-
-### Minor Changes
-
-- 08b9cd32: Upgrade to Effection v2 beta
--- packages/performance/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "@bigtest/performance" {
-  var performance: Performance;
-
-  var PerformanceObserver: {
-    prototype: PerformanceObserver;
-    new(callback: PerformanceObserverCallback): PerformanceObserver;
-    readonly supportedEntryTypes: ReadonlyArray<string>;
-  }
-}
